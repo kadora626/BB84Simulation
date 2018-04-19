ebe81e2dd8d7009d6004cf38f9823a5166a41127
kadora <k.kadora@gmail.com>
2018-04-19 23:49:34 +0900

    init project

---
+++ Driver.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace BB84Simulation
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Operation.qs
@@ -0,0 +1,13 @@
+﻿namespace BB84Simulation
+{
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Primitive;
+
+    operation Operation () : ()
+    {
+        body
+        {
+            
+        }
+    }
+}
