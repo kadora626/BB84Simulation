4dd77c3a866133afb07e7059da4781266198efae
kadora <k.kadora@gmail.com>
2018-04-22 22:33:13 +0900

    feat: Fix key length and add eavesdropper check

--- Driver.cs
+++ Driver.cs
@@ -9,10 +9,28 @@ namespace BB84Simulation
         {
             using (var sim = new QuantumSimulator())
             {
-                var res = BB84.Run(sim, 20).Result;
-                var (AliceKey, BobKey) = res;
-                System.Console.WriteLine($"AliceKey: {AliceKey}");
-                System.Console.WriteLine($"BobKey:   {BobKey}");
+                var AliceKey = "";
+                var BobKey = "";
+                var IsEavesdropper = "There is an eavesdropper.";
+                var KeyLength = 20;
+                var CheckLength = 10;
+                var StepBits = 20;
+                while (AliceKey.Length < KeyLength + CheckLength){
+                    var res = BB84.Run(sim, StepBits).Result;
+                    var (AliceReceives, BobReceives) = res;
+                    AliceKey = AliceKey + AliceReceives;
+                    BobKey = BobKey + BobReceives;
+                }
+                var AliceCheckBits = AliceKey.Substring(KeyLength);
+                var BobCheckBits = BobKey.Substring(KeyLength);
+                if (AliceCheckBits == BobCheckBits) {
+                    IsEavesdropper = "There is no eavesdropper.";
+                }
+                System.Console.WriteLine(IsEavesdropper);
+                System.Console.WriteLine($"AliceKey:       {AliceKey.Substring(0,KeyLength)}");
+                System.Console.WriteLine($"BobKey:         {BobKey.Substring(0,KeyLength)}");
+                System.Console.WriteLine($"AliceCheckBits: {AliceCheckBits}");
+                System.Console.WriteLine($"BobCheckBits:   {BobCheckBits}");
             }
 
             System.Console.WriteLine("\n\nPress Enter to continue...\n\n");
