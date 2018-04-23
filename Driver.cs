087ed603176dafb834580d840fbf0a978d4d4bf9
kadora <k.kadora@gmail.com>
2018-04-23 19:53:12 +0900

    feat: Devide pattern and Add counter

--- Driver.cs
+++ Driver.cs
@@ -5,19 +5,16 @@ namespace BB84Simulation
 {
     class Driver
     {
-        static void Main(string[] args)
-        {
+        private static bool SimulateBB84(int KeyLength, int CheckLength, int StepBits, bool Eavesdropper) {
+            var result = true;
             using (var sim = new QuantumSimulator())
             {
                 var AliceKey = "";
                 var BobKey = "";
                 var EveKey = "";
-                var IsEavesdropper = "WARNING!! There is an eavesdropper.";
-                var KeyLength = 20;
-                var CheckLength = 1;
-                var StepBits = 20;
+                var EavesdropMessage = "WARNING!! There is an eavesdropper.";
                 while (AliceKey.Length < KeyLength + CheckLength){
-                    var res = BB84.Run(sim, StepBits, true).Result;
+                    var res = BB84.Run(sim, StepBits, Eavesdropper).Result;
                     var (AliceReceives, BobReceives, EveReceives) = res;
                     AliceKey = AliceKey + AliceReceives;
                     BobKey = BobKey + BobReceives;
@@ -26,17 +23,46 @@ namespace BB84Simulation
                 var AliceCheckBits = AliceKey.Substring(KeyLength, CheckLength);
                 var BobCheckBits = BobKey.Substring(KeyLength, CheckLength);
                 if (AliceCheckBits == BobCheckBits) {
-                    IsEavesdropper = "There is no eavesdropper.";
+                    EavesdropMessage = "There is no eavesdropper.";
+                    result = false;
                 }
-                System.Console.WriteLine(IsEavesdropper);
+                System.Console.WriteLine(EavesdropMessage);
                 System.Console.WriteLine($"AliceKey:       {AliceKey.Substring(0,KeyLength)}");
                 System.Console.WriteLine($"BobKey:         {BobKey.Substring(0,KeyLength)}");
-                System.Console.WriteLine($"EveKey:         {EveKey.Substring(0,KeyLength)}");
+                if (Eavesdropper) {
+                    System.Console.WriteLine($"EveKey:         {EveKey.Substring(0,KeyLength)}");
+                }
                 System.Console.WriteLine($"AliceCheckBits: {AliceCheckBits}");
                 System.Console.WriteLine($"BobCheckBits:   {BobCheckBits}");
             }
+            System.Console.WriteLine("\n\n");
+            return result;
+        }
 
-            System.Console.WriteLine("\n\nPress Enter to continue...\n\n");
+        static void Main(string[] args)
+        {
+            var KeyLength = 5;
+            var CheckLength = 5;
+            var StepBits = 10;
+            // Pattern with no eavasdropper
+            System.Console.WriteLine("PART1:");
+            SimulateBB84(KeyLength,CheckLength,StepBits,false);
+
+            // Pattern with eavasdropper
+            System.Console.WriteLine("PART2:");
+            var Steps = 100;
+            var CountEavesdrop = 0;
+            var CountNonEavesdrop = 0;
+            for (int step = 0; step < Steps; step++) {
+                System.Console.WriteLine($"STEP-{Step}");
+                if(SimulateBB84(KeyLength,CheckLength,StepBits,true)){
+                    CountEavesdrop++;
+                } else {
+                    CountNonEavesdrop++;
+                }
+            }
+            System.Console.WriteLine($"Total:{Steps}, Eavesdrop:{CountEavesdrop}, NoEavesdrop:{CountNonEavesdrop}");
+            System.Console.WriteLine("Press Enter to continue...\n\n");
             System.Console.ReadLine();
         }
     }
